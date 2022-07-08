fd11430230185e870283eeb6a09507ca62b10010
schmid42 <schmidp@edith-stein-schule.net>
2022-07-08 08:50:51 +0200

    implemented config file configuration

--- chocolatey/uninstall.ps1
+++ chocolatey/uninstall.ps1
@@ -7,11 +7,14 @@
    Creation Date:  25.03.2022
 #>
 
+# load configuration file
+$config = Get-Content -Path ./config.json | ConvertFrom-Json
+
 # the url of the REST api for log messages
-$urlLog   = "http://192.168.105.25:1337/api/logs"
+$urlLog   = $config.urlLog
 
 # the software list to install
-$listPath = "\\ESAUSRV01\NETLOGON\chocolatey\uninstall.txt"
+$listPath = $config.listPaths.uninstall
 
 # get actual timestamp
 $now = Get-Date -Format "o"
@@ -22,7 +25,7 @@ $computername = $computername.ToLower()
 
 $headers = @{
     'Content-Type'  = 'application/json; charset=utf-8';
-    'Authorization' = 'Bearer 8f6072a9b8f462bf83b4f7831abc4020e9e8d69fa024cf011174faf4b853e480a9b05afe2d87686a0dd7fecad07d727f89fe61691309cc28e6338252fd60b13a1d28a7a3325445f41439d4877effd706c2a24ef8a38c1625cffca610f5de23b20a07e71ba1525964fb96ee7eb860da322dadd20e3733e865050354127ff92a3c'
+    'Authorization' = $config.token
 }
 
 # read the software list from file
